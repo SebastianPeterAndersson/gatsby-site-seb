4467cfa345dc017b5f4041606b8e8f3c6e184e0d
Sebastian Andersson <sebastianpeterandersson@gmail.com>
2022-07-30 09:15:45 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/components/layout.js
@@ -0,0 +1,39 @@
+
+
+import * as React from 'react'
+import { Link } from 'gatsby'
+import
+    {container, navigation, heading}
+        from "../components/layout.module.css"
+const Layout = ({ pageTitle, children }) => {
+
+  return (
+
+    <div className={container}>
+
+     <title>{pageTitle}</title>
+
+      <nav>
+
+        <ul className={navigation}>
+
+          <li><Link to="/">Home</Link></li>
+
+          <li><Link to="/about">About</Link></li>
+
+        </ul>
+
+      </nav>
+
+      <main>
+
+         <h1 className={heading}>{pageTitle}</h1>
+
+        {children}
+
+      </main>
+
+    </div>
+  )
+}
+export default Layout
---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+
+
+import * as React from 'react'
+
+import Layout from "../components/layout"
+
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hello, you. This is Sebastian speaking.
+      How very nice of you to stop by.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage

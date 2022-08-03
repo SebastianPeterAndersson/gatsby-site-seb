50ba33debffff8eb7161d55d3ed3409a2ed99c8d
Sebastian Andersson <sebastianpeterandersson@gmail.com>
2022-08-03 15:47:15 +0200

    Added articles with MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,6 @@ module.exports = {
         path: `${__dirname}/articles`,
       }
     },
+    "gatsby-plugin-mdx",
   ],
 }
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,38 +1,47 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const ArticlePage = ({data}) => {
-
-const article = data.allFile.nodes
 
 
   return (
 
-
-
     <Layout pageTitle="Articles">
     {
-        article.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
-      <p>My cool posts will go in here</p>
+       data.allMdx.nodes.map((node) => (
+         <article key={node.id}>
+           <h2>{node.frontmatter.title}</h2>
+           <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+                { node.body }
+            </MDXRenderer>
+         </article>
+       ))
+     }
     </Layout>
   )
 
 }
 
+
 export const query = graphql`
-query {
-  allFile(filter: {sourceInstanceName: {eq: "articles"}}) {
-    nodes {
-      name
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date
+          title
+        }
+        id
+        body
+      }
     }
   }
-}
 `
+
+
+
 
 export default ArticlePage

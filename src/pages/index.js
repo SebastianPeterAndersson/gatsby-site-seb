c0444917e84d0605047a7d6d3c0f02c07bcf72c9
Sebastian Andersson <sebastianpeterandersson@gmail.com>
2022-08-22 12:42:18 +0200

    Under Construction Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,18 +11,7 @@ import {StaticImage} from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>Welcome to my homepage. It's really nice seeing you!</p>
-      <p>Click <Link to="/about">here</Link> to learn more about me, you handsome
-      son of a gun!</p>
-      <p>Or, take a look at this nice swan:</p>
-      <StaticImage
-      alt="A swan in nice water"
-      src="https://cdn.pixabay.com/photo/2017/02/18/13/55/swan-2077219_1280.jpg">
-      </StaticImage>
-      <p>Have a great day!</p>
-      <p><b>Seb</b></p>
-    </Layout>
+     <h1>Under Construction</h1>
   )
 }
 

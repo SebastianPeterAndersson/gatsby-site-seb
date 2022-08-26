ae45d3a307db3df9bf66cdd4fc71fe89d6c89027
Sebastian Andersson <sebastianpeterandersson@gmail.com>
2022-08-26 15:05:50 +0200

    Created a new post

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,42 +5,22 @@ module.exports = {
   },
   plugins: [
       "gatsby-plugin-image",
-      "gatsby-plugin-sharp",{
+      "gatsby-plugin-sharp",
+    {
       resolve: "gatsby-source-filesystem",
-      options: {
+        options: {
         name: `articles`,
         path: `${__dirname}/articles`,
-      }
+       }
     },
     "gatsby-plugin-mdx",
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-source-contentful`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "GA-TRACKING_ID", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "UA-238060727-1",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "https://astian.se",
-        },
+        spaceId: `4w7noc229lbu`,
+        accessToken: `F48116NFIO49jMtP9lrQmuTzWpGcE1ZNYNUc4nKYbB4`,
+        host: `preview.contentful.com`,
       },
-    },
+    }
   ],
 }
